749eddaa11162fc052176d1c5ffff6ef6f1d42a1
your name <your email>
2019-01-05 18:06:22 +0800

    gg

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>
+  </div>
 </template>
 
 <script>
+import MainTabBar from "./components/content/mainTabbar/MainTabBar";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    MainTabBar
+  }
 };
 </script>
 
 <style>
-@import "./assets/css/base.css";
+@import "assets/css/base.css";
 </style>
